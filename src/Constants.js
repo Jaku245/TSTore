adf916b90129e8755ca929497500ab7a8e33df3b
JdJaku <jaimin.desai@onboardops.com>
2023-04-11 15:58:22 -0300

    Using Amplify env variables.

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
 // export const BACKEND_URL = 'http://tstore.us-east-1.elasticbeanstalk.com';
-// export const API_GATEWAY_URL = 'https://6g99ykmr3h.execute-api.us-east-1.amazonaws.com/prod/addOrderToQueue';
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+// export const API_GATEWAY_URL = 'https://6g99ykmr3h.execute-api.us-east-1.amazonaws.com/prod/addOrderToQueue';
 export const API_GATEWAY_URL = process.env.REACT_APP_API_GATEWAY_URL;
